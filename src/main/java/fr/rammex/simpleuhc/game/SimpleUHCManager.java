f7d8f54404da49ebbbfe8b0a885caaec4afd692f
rammex <104463176+rammex666@users.noreply.github.com>
2025-09-19 10:58:49 +0200

    start task

--- src/main/java/fr/rammex/simpleuhc/game/SimpleUHCManager.java
+++ src/main/java/fr/rammex/simpleuhc/game/SimpleUHCManager.java
@@ -4,21 +4,15 @@ import api.rammex.gameapi.GameAPI;
 import api.rammex.gameapi.role.Role;
 import api.rammex.gameapi.scenario.AbstractScenario;
 import api.rammex.gameapi.task.AbstractTask;
-import fr.rammex.simpleuhc.option.OptionSetup;
-import fr.rammex.simpleuhc.task.MainTask;
-import fr.rammex.simpleuhc.task.MeetupTask;
-import fr.rammex.simpleuhc.task.PvpTask;
+import fr.rammex.simpleuhc.task.StartTask;
 import fr.rammex.simpleuhc.world.WorldManager;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SimpleUHCManager extends AbstractScenario {
-    private AbstractTask mainTask = new MainTask();;
-    private AbstractTask meetupTask = new MeetupTask((int) OptionSetup.getOption("Game Meetup").getValue());;
-    private AbstractTask pvpTask = new PvpTask((int) OptionSetup.getOption("Game PvP").getValue());;
+    private AbstractTask startTask = new StartTask(30);
+    private List<Role> roles = new ArrayList<>();
     public static boolean pvpEnabled = false;
     public static boolean isGameRunning = false;
     public SimpleUHCManager() {
@@ -32,7 +26,7 @@ public class SimpleUHCManager extends AbstractScenario {
 
     @Override
     public List<Role> getAvailableRoles() {
-        return Collections.emptyList();
+        return roles;
     }
 
     @Override
@@ -40,19 +34,7 @@ public class SimpleUHCManager extends AbstractScenario {
         isGameRunning = true;
         WorldManager.createWorld();
 
-        GameAPI.instance.getTaskManager().addTask(mainTask);
-        GameAPI.instance.getTaskManager().startTask(mainTask);
-
-        GameAPI.instance.getTaskManager().addTask(meetupTask);
-        GameAPI.instance.getTaskManager().startTask(meetupTask);
-
-        GameAPI.instance.getTaskManager().addTask(pvpTask);
-        GameAPI.instance.getTaskManager().startTask(pvpTask);
-
-        for(Player p : Bukkit.getOnlinePlayers()) {
-            WorldManager.teleportPlayer(p);
-            p.sendMessage("§eL'UHC a commencé ! Préparez-vous !");
-        }
+        GameAPI.instance.getTaskManager().startTask(startTask);
 
     }
 
@@ -61,8 +43,9 @@ public class SimpleUHCManager extends AbstractScenario {
         isGameRunning = false;
         pvpEnabled = false;
 
-        GameAPI.instance.getTaskManager().stopTask(mainTask);
-        GameAPI.instance.getTaskManager().stopTask(meetupTask);
-        GameAPI.instance.getTaskManager().stopTask(pvpTask);
+        GameAPI.instance.getTaskManager().stopTask(startTask);
+        GameAPI.instance.getTaskManager().stopTask(StartTask.getMainTask());
+        GameAPI.instance.getTaskManager().stopTask(StartTask.getMeetupTask());
+        GameAPI.instance.getTaskManager().stopTask(StartTask.getPvpTask());
     }
 }
---
+++ src/main/java/fr/rammex/simpleuhc/task/StartTask.java
@@ -0,0 +1,53 @@
+package fr.rammex.simpleuhc.task;
+
+import api.rammex.gameapi.GameAPI;
+import api.rammex.gameapi.task.AbstractTask;
+import fr.rammex.simpleuhc.option.OptionSetup;
+
+public class StartTask extends AbstractTask {
+    private static AbstractTask mainTask = new MainTask();;
+    private static AbstractTask meetupTask = new MeetupTask((int) OptionSetup.getOption("Game Meetup").getValue());
+    private static AbstractTask pvpTask = new PvpTask((int) OptionSetup.getOption("Game PvP").getValue());
+
+    public StartTask(int startDelay) {
+        super("MainTask", "Main task for SimpleUHC", startDelay);
+    }
+
+    @Override
+    protected void onStart() {
+    }
+
+    @Override
+    protected void onTick() {
+        setActualDuration(getActualDuration()+1);
+    }
+
+    @Override
+    protected void onFinish() {
+        GameAPI.instance.getTaskManager().addTask(mainTask);
+        GameAPI.instance.getTaskManager().startTask(mainTask);
+
+        GameAPI.instance.getTaskManager().addTask(meetupTask);
+        GameAPI.instance.getTaskManager().startTask(meetupTask);
+
+        GameAPI.instance.getTaskManager().addTask(pvpTask);
+        GameAPI.instance.getTaskManager().startTask(pvpTask);
+    }
+
+    @Override
+    protected void onStop() {
+        setActualDuration(0);
+    }
+
+    public static AbstractTask getMainTask() {
+        return mainTask;
+    }
+
+    public static AbstractTask getMeetupTask() {
+        return meetupTask;
+    }
+
+    public static AbstractTask getPvpTask() {
+        return pvpTask;
+    }
+}
